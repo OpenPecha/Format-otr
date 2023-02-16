2bf5a88d46ec13bae74d1c46edf14a3eb9bc3c86
sonam phuntsog <spsither@gmail.com>
2023-02-16 20:36:11 +0530

    cli for format_otr

--- src/format_otr/format_otr.py
+++ src/format_otr/format_otr.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 
 def add_breaks_on_timestamps(text):
@@ -14,3 +15,8 @@ def get_format_otr(file_path_to_otr):
     no_breaks_otr_content = remove_all_breaks(otr_file_content)
     format_otr = add_breaks_on_timestamps(no_breaks_otr_content)
     return format_otr.replace("\xa0", " ")
+
+
+def main_cli():
+    if len(sys.argv) > 1 and sys.argv[1][-4:] == ".otr":
+        print(get_format_otr(sys.argv[1]))
