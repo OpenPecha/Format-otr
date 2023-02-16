0bf5287e5f8f70e6fa1aedc9417a0ab631852ad2
sonam phuntsog <spsither@gmail.com>
2023-02-16 14:41:19 +0530

    simple replace to format otr

--- src/format_otr/format_otr.py
+++ src/format_otr/format_otr.py
@@ -1,2 +1,16 @@
-def add_one(number):
-    return number + 1
+from pathlib import Path
+
+
+def add_breaks_on_timestamps(text):
+    return text.replace("<span", "<p><br /></p> <span")
+
+
+def remove_all_breaks(text):
+    return text.replace("<p><br /></p>", " ")
+
+
+def get_format_otr(file_path_to_otr):
+    otr_file_content = Path(file_path_to_otr).read_text("utf8")
+    no_breaks = remove_all_breaks(otr_file_content)
+    format_otr = add_breaks_on_timestamps(no_breaks)
+    return format_otr.replace("\xa0", " ")
