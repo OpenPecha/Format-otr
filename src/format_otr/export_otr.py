85d25ee2734adf19a92a500362ebc1c6d604ab71
sonam phuntsog <spsither@gmail.com>
2023-02-16 17:48:49 +0530

    test pre-commit

--- src/format_otr/export_otr.py
+++ src/format_otr/export_otr.py
@@ -1,9 +1,11 @@
 from format_otr.utils import get_json_from_file, parse_html, get_all_timestamps
 from pathlib import Path
 
+import sys
+
 
 def get_text_between(start, end):
-    text = ''
+    text = ""
     tag = start.next_element.next_element
     while tag != end:
         if tag.name is None:
@@ -11,8 +13,9 @@ def get_text_between(start, end):
         tag = tag.next_element
     return text
 
+
 def get_text_till_end(start):
-    text = ''
+    text = ""
     tag = start.next_element.next_element
     while tag:
         if tag.name is None:
@@ -20,16 +23,22 @@ def get_text_till_end(start):
         tag = tag.next_element
     return text
 
+
 def get_txt(file_path_to_otr):
     otr_file = Path(file_path_to_otr)
     json_otr = get_json_from_file(otr_file)
-    soup = parse_html(json_otr['text'])
+    soup = parse_html(json_otr["text"])
     timestamps = get_all_timestamps(soup)
-    txt = ''
+    txt = ""
     for start, end in zip(timestamps, timestamps[1:]):
         txt += f"{start.string} {get_text_between(start, end).strip()}\n"
     txt += f"{timestamps[-1].string} {get_text_till_end(timestamps[-1]).strip()}"
     # \xa0 is non-breaking space. Replace it with space.
-    txt = txt.replace(u'\xa0', u' ')
+    txt = txt.replace("\xa0", " ")
     return txt
-    
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1][-4:] == ".otr":
+        # print(get_txt(sys.argv[1]))
+        # print(sys.argv[1])
--- src/format_otr/format_otr.py
+++ src/format_otr/format_otr.py
@@ -2,15 +2,31 @@ from pathlib import Path
 
 
 def add_breaks_on_timestamps(text):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return text.replace("<span", "<p><br /></p> <span")
 
 
 def remove_all_breaks(text):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return text.replace("<p><br /></p>", " ")
 
 
 def get_format_otr(file_path_to_otr):
     otr_file_content = Path(file_path_to_otr).read_text("utf8")
-    no_breaks = remove_all_breaks(otr_file_content)
-    format_otr = add_breaks_on_timestamps(no_breaks)
+    no_breaks_otr_content = remove_all_breaks(otr_file_content)
+    format_otr = add_breaks_on_timestamps(no_breaks_otr_content)
     return format_otr.replace("\xa0", " ")
